4ed390ec01443f6f638473fd0e041e86cdfae79b
Srabani <srabaniupadhyaya@yahoo.com>
2025-01-26 19:12:07 +0000

    reporter basics

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,15 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testMatch:["tests/login.test.ts"],
     use:{
-        headless:false
-    }
+        headless:false,
+        screenshot:"on",
+        video:"on"
+    },
+
+    reporter:[["dot"],["json",{
+        outputFile:"jsonReports/jsonReport.json"
+    }],["html",{
+        open:"never"
+    }]]
+      
 });
