9698de519928ac97b0b5c4042898e6b440761e51
Srabani <srabaniupadhyaya@yahoo.com>
2025-11-15 18:35:02 +0000

    fixture comments

--- e2e/conduit-app-test/fixtures/pomFixtures.ts
+++ e2e/conduit-app-test/fixtures/pomFixtures.ts
@@ -3,16 +3,18 @@ import { SignInPage } from '../page-objects/signInPage';
 import {InventoryPage} from "../page-objects/inventoryPage";
 import BasePage from "../page-objects/basePage";
 
-
+// Defining a Type for Fixture
 type pages = {
     basePage: BasePage;
     signInPage: SignInPage;
     inventoryPage: InventoryPage;
 }
 
+// Extending the Base Test with custom Fixtures
 const testPages = baseTest.extend<pages>({
     signInPage: async ({page},use) => {
-        await use(new SignInPage(page));
+        await use(new SignInPage(page)); // Passes it to use() so it becomes available tests.
+
     },
     inventoryPage: async ({page},use) => {
         await use(new InventoryPage(page));
@@ -22,5 +24,6 @@ const testPages = baseTest.extend<pages>({
     }
 })
 
+// Exporting the Customized Test and Expect
 export const test = testPages;
 export const expect = testPages.expect;
